96e0aa2020b333564f2539166b0cb33408e1a8ba
eslavi <eslavi_jonhas@hotmail.com>
2023-05-10 11:16:53 -0600

    Heroes-list done, added images for the heroes

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of, pipe } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -14,5 +14,12 @@ export class HeroesService {
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseURL }/heroes`);
   }
-  
+
+  getHeroById(id: string): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${ this.baseURL }/heroes/${id}`)
+      .pipe(
+        catchError(error => of(undefined))
+      );
+  }
+
 }
